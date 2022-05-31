06da249bba5020b582ff6a8035a9bc290073e51a
HankPeeples <hgpeeples48@gmail.com>
2022-05-31 18:20:49 -0500

    Added logic for initial GET request and parsing the response

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/hankpeeples/sitemapBuilder
 
 go 1.18
+
+require (
+	github.com/hankpeeples/linkParser v0.0.0-20220531185255-b0654e140bfb // indirect
+	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
 
+	link "github.com/hankpeeples/linkParser"
+)
+
+func main() {
+	urlFlag := flag.String("url", "https://gophercises.com", "the url you want to build a sitemap for")
+	flag.Parse()
+
+	resp, err := http.Get(*urlFlag)
+	if err != nil {
+		panic(err)
+	}
+	// close resp.Body after function has finished (in this case, closes after main finishes)
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
+
+	links, _ := link.Parse(resp.Body)
+
+	for _, l := range links {
+		fmt.Println(l)
+	}
 }
